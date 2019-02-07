131db97c791f41c8efd7802103789746192ab352
Joshua Cooper <jsc115@sprite33.doc.ic.ac.uk>
2019-02-07 13:16:39 +0000

    Added threat scores to config

--- config.py
+++ config.py
@@ -1,3 +1,16 @@
 # Includes parameters to determine sensitivity of the system
 
 SQLI_SENSITIVITY = 3 # A value between 1-lowest & 3-highest
+
+# 1-10 Scale
+THREAT_SCORES = {
+	'no_referrer': 1, 
+	'xss': 9,
+	'sql_injection': 10,
+	'remote_file_inclusion': 7,
+	'scanning_tools': 5,
+	'no_user_agent': 6,
+	'fake_bot': 4,
+	'blacklisted_address': 3,
+	'ddos': 8
+}
